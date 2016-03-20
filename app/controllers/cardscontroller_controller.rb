5f6ec9d283ba719cfe2cffb792aa394fb756446b
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-20 15:02:00 +0300

    add tips of houndi

--- app/controllers/cardscontroller_controller.rb
+++ app/controllers/cardscontroller_controller.rb
@@ -1,10 +1,11 @@
 class CardscontrollerController < ApplicationController
 
-	def index
-		@cards = Card.all
-	end
-
-	def show
-		@card = Card.find(parms[:id])
-	end
+  def index
+    @cards = Card.all
+  end
+  
+  def show
+    @card = Card.find(parms[:id])
+  end
+  
 end
