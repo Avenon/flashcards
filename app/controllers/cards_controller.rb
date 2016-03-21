0b6f783f9b355b8f29b2cb7900444d5748a3b9ed
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-22 00:28:42 +0300

    add crud code

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,36 @@
 class CardsController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
-
-  def show
-    @card = Card.find(parms[:id])
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
   def index
     @cards = Card.all
   end
 
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.create(params.require(:card).permit(:original_text, :translated_text, :review_date))
+    redirect_to @card
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update_attributes(params.require(:card).permit(:original_text, :translated_text, :review_date))
+    redirect_to @card
+  end
+
   def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
   end
 
 end
