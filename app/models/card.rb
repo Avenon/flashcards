39aaf6c4371628699d71d20ead3a104c2bb2775b
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-22 22:53:32 +0300

    Add validate

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
+  validates :translated_text, exclusion: { in: :original_text, message: "Must be different with original text" }
 end
