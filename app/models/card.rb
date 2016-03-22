ba93c7c39b3d2a525e4ffb074ffe843001b27bf6
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-23 00:10:36 +0300

    Add bootrtrap to simple_form

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,5 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
   validates :translated_text, exclusion: { in: :original_text, message: "Must be different with original text" }
+  # <%= f.input :original_text, label: 'Оригинальный текст', label_html: {class: 'col-sm-2 control-label'}, :required => false, input_html: { class: "form-control"} %><br>
 end
