e2945a85fe174f984aae06a812fa1e59b8deeac0
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-23 06:49:41 +0300

    Add private method get_card_params

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,7 +13,8 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card = Card.create(params.require(:card).permit(:original_text, :translated_text, :review_date))
+    @card_params = get_card_params
+    @card = Card.create(@card_params)
 
     if @card.errors.empty?
       redirect_to @card
@@ -27,8 +28,9 @@ class CardsController < ApplicationController
   end
 
   def update
+    @card_params = get_card_params
     @card = Card.find(params[:id])
-    @card.update_attributes(params.require(:card).permit(:original_text, :translated_text, :review_date))
+    @card.update_attributes(@card_params)
 
     if @card.errors.empty?
       redirect_to @card
@@ -43,4 +45,8 @@ class CardsController < ApplicationController
     redirect_to cards_path
   end
 
+  private
+  def get_card_params
+    @card_params = params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
 end
