f5bf007b6894e27d8bddbf10922a0d758b5f06a3
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-23 13:54:38 +0300

    add changes in card_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,12 @@
 class CardsController < ApplicationController
+  before_action :find_card, only: [:show, :edit, :update, :destroy]
 
   def index
     @cards = Card.all
   end
 
   def show
-    @card = Card.find(params[:id])
+    #@card = Card.find(params[:id])
   end
 
   def new
@@ -13,24 +14,23 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card_params = get_card_params
-    @card = Card.create(@card_params)
+    @card = Card.create(get_card_params)
 
-    if @card.errors.empty?
-      redirect_to @card
+    if @card.save
+      redirect_to cards_path
     else
       render "new"
     end
   end
 
   def edit
-    @card = Card.find(params[:id])
+    #@card = Card.find(params[:id])
   end
 
   def update
-    @card_params = get_card_params
-    @card = Card.find(params[:id])
-    @card.update_attributes(@card_params)
+
+    #@card = Card.find(params[:id])
+    @card.update_attributes(get_card_params)
 
     if @card.errors.empty?
       redirect_to @card
@@ -40,13 +40,18 @@ class CardsController < ApplicationController
   end
 
   def destroy
-    @card = Card.find(params[:id])
+   # @card = Card.find(params[:id])
     @card.destroy
     redirect_to cards_path
   end
 
   private
+
   def get_card_params
-    @card_params = params.require(:card).permit(:original_text, :translated_text, :review_date)
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
