fa6eb7760468460c0186800519d41d4d02ddd619
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-24 16:20:30 +0300

    add mb_chars

--- app/validators/compare_strings.rb
+++ app/validators/compare_strings.rb
@@ -2,7 +2,7 @@ class CompareStrings < ActiveModel::Validator
   def validate(record)
     return if record.original_text.blank? || record.translated_text.blank?
 
-    if record.original_text.casecmp(record.translated_text) == 0
+    if record.original_text.mb_chars.casecmp(record.translated_text.mb_chars) == 0
       record.errors[:base] << "Must be different"
     end
   end
