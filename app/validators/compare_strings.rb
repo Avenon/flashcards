9e338ad2dc1cd8f2bac059510f3a3250d75f104b
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-24 15:35:47 +0300

    add houdci tips

--- app/validators/compare_strings.rb
+++ app/validators/compare_strings.rb
@@ -1,11 +1,9 @@
 class CompareStrings < ActiveModel::Validator
-
   def validate(record)
-    return if record.original_text.blank? or record.translated_text.blank?
+    return if record.original_text.blank? || record.translated_text.blank?
 
     if record.original_text.mb_chars.downcase == record.translated_text.mb_chars.downcase
       record.errors[:base] << "Must be different"
     end
   end
-
 end
