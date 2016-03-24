f375f79704f71f056747e96e083a4305e098beeb
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-24 15:28:19 +0300

    Add validator to app

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
-  validates :translated_text, exclusion: { in: :original_text, message: "Must be different with original text" }
+  validates_with CompareStrings
 end
---
+++ app/validators/compare_strings.rb
@@ -0,0 +1,11 @@
+class CompareStrings < ActiveModel::Validator
+
+  def validate(record)
+    return if record.original_text.blank? or record.translated_text.blank?
+
+    if record.original_text.mb_chars.downcase == record.translated_text.mb_chars.downcase
+      record.errors[:base] << "Must be different"
+    end
+  end
+
+end
