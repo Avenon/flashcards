c26aaf01382eda75788e989428935c5f3dccbc21
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-25 12:31:50 +0300

    Add conditions to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    array = Card.all.map { |i| i.id }
+    array = Card.where('review_date <= ?', Time.now).map { |i| i.id }
     card_id = array[rand(array.size)]
     @card = Card.find(card_id)
   end
