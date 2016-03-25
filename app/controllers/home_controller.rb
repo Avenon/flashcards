7171e1d6f66dff9102f785eec2e7d828942ff7c0
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-25 18:55:26 +0300

    add houdchi recommend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
   def checktranslate
     @home_params = get_home_params
     @card = Card.find(@home_params[:id])
-    @card.update_attributes(:id => @home_params[:id], :original_text => @home_params[:original_text], :translated_text => @home_params[:translated_text], :review_date => @home_params[:review_date])
+    @card.update_attributes(id: @home_params[:id], original_text: @home_params[:original_text], translated_text: @home_params[:translated_text], review_date: @home_params[:review_date])
 
     render @card.checkusertranslate(@home_params[:usertext], @home_params[:original_text])
   end
