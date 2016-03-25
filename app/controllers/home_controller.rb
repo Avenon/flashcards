53983686d886609df3f1e4702ca58be31fd4ea50
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-25 12:23:30 +0300

    Add houdchi recommend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    array = Array.new
-    array = Card.all.map { |i| i.id}
+    array = Card.all.map { |i| i.id }
     card_id = array[rand(array.size)]
     @card = Card.find(card_id)
   end
