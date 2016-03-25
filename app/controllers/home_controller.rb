10b0bbb725ec9e02b487d1edd4e8c49b3cdba6b4
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-25 17:21:57 +0300

    Add templates, modified homecontrollers, card model, config routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,20 @@
 class HomeController < ApplicationController
 
   def index
-    array = Card.where('review_date <= ?', Time.now).map { |i| i.id }
-    card_id = array[rand(array.size)]
-    @card = Card.find(card_id)
+    @card_id = Card.find_random_card
+    @card = Card.find(@card_id)
+  end
+
+  def checktranslate
+    @user_check = params.require(:home).permit(:usertext)
+    @card_update = params.require(:home).permit(:id, :original_text, :translated_text, :review_date )
+    @card = Card.find(@card_update[:id])
+    @card.update_attributes(@card_update)
+
+    if @user_check[:usertext].downcase == @card_update[:original_text].downcase
+      render "success"
+    else
+      render "error"
+    end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,11 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
   validates_with CompareStrings
+
+  def self.find_random_card
+    array = Card.check_date.map(&:id)
+    array[rand(array.size)]
+  end
+
+  scope :check_date, -> { where('review_date <= ?', Time.now) }
 end
