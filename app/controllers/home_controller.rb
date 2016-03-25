1eebbd7abdf5f968056595612103808f440d9e8e
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-25 12:15:57 +0300

    Add home_controller and modified index template for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+    array = Array.new
+    array = Card.all.map { |i| i.id}
+    card_id = array[rand(array.size)]
+    @card = Card.find(card_id)
+  end
 end
