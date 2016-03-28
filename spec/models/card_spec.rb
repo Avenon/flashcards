e4bd44eba9b953b369ab5279a8d158f94f697235
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-28 22:14:49 +0300

    Add 3 spec tests to project

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,8 +1,18 @@
-require 'spec_helper'
+require "rails_helper"
 
-describe Card
-  it "user text should be equal to original text " do
+RSpec.describe Card, :type => :model do
+  it "user text should be equal to original text" do
     card = Card.new(original_text: "house")
-    card.check_translate("house").should == true
+    expect(card.check_translate("house")) == true
+  end
+
+  it "user text with capslock should be equal to original text" do
+    card = Card.new(original_text: "HoUsE")
+    expect(card.check_translate("hOuSe")) == true
+  end
+
+  it "review date must be increase to 3 day" do
+    card = Card.new(review_date: Time.now)
+    expect(card.increase_review_date) == Time.now + 3.days
   end
 end
