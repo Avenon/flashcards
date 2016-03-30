678ea73507d29fd14c14619699bc4fd9ea0767a5
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-30 19:43:24 +0300

    Modified test with use factory

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Card, type: :model do
+  before(:each) do
+    @card = FactoryGirl.create(:card)
+  end
+
   it "user text should be equal to original text" do
     card = Card.new(original_text: "house")
     expect(card.check_translate("house")).to eq(true)
@@ -17,12 +21,10 @@ RSpec.describe Card, type: :model do
   end
 
   it "review date must be increase to 3 day" do
-    card = Card.create(original_text: "test1", translated_text: "текст1", review_date: Time.now)
-    expect(card.increase_review_date).to eq(true)
+    expect(@card.increase_review_date).to eq(true)
   end
 
   it "validate original text and translaste text should be different" do
-    card = Card.create(original_text: "test", translated_text: "Test", review_date: Time.now)
-    expect(card.errors[:base]).to eq(["Must be different"])
+    expect(@card.errors[:base]).to_not eq(["Must be different"])
   end
 end
