d7b66c6da46a802955ba2feacaca3c83ed6cb3d3
Egor G. Silaev <silaevegor@hotmail.com>
2016-03-31 13:27:31 +0300

    modified feature spec

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -3,24 +3,19 @@ require "rails_helper"
 describe 'Check card translate' do
 
   let!(:card) { FactoryGirl.create(:card) }
-  #before(:each) do
-  #  @card = FactoryGirl.create(:card)
-  #end
 
   it "correct translation must be show 'Успех'" do
-    puts card.id
-    @card = Card.find(card.id)
-    puts @card
-    #unless @card.exists?
-    #  puts "test1"
-    #end
     visit root_path
-    #expect(page).to have_content('Карточки кончились')
-    puts page.body
-    #page.find('h3').to eq('Карточки кончились')
-    #fill_in "text", with: 'test1'
-    #click_button('Проверить')
-    #expect(card.check_translate("test1")).to eq(true)
-    #expect(page.find('h3/text()')).to eq(['Карточки кончились'])
+    # puts page.body Для отладки в capybara
+    fill_in "home_usertext", with: 'test1'
+    click_button('Проверить')
+    expect(page).to have_content('Успех! Перевод верен')
+  end
+
+  it "incorrect translation must be show 'Ошибка'" do
+    visit root_path
+    fill_in "home_usertext", with: 'test2'
+    click_button('Проверить')
+    expect(page).to have_content('Ошибка! Перевод ошибочный')
   end
 end
