073eed2d4b65f11fe0980dfde7260e388d63ac78
Egor G. Silaev <silaevegor@hotmail.com>
2016-04-02 14:57:22 +0300

    Add spec test

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -1,11 +1,20 @@
 require "rails_helper"
 
 describe 'Check card translate' do
-  let!(:card) { FactoryGirl.create(:card) }
+  let!(:user) { create(:user, email: "test@test.ru", password: "test", password_confirmation: "test") }
+  let!(:card) { FactoryGirl.create(:card, user_id: user.id) }
+
+  before(:each) do
+    visit root_path
+    click_link "Войти"
+    fill_in :email, with: "test@test.ru"
+    fill_in :password, with: "test"
+    click_button "Войти"
+  end
 
   it "correct translation must be show 'Успех'" do
     visit root_path
-    # puts page.body Для отладки в capybara
+    #puts page.body Для отладки в capybara
     fill_in "home_usertext", with: 'test1'
     click_button('Проверить')
     expect(page).to have_content('Успех! Перевод верен')
