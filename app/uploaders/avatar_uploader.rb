6caa1c02323ebc1a7a31f587b8f0ee357bbdcb2d
Egor G. Silaev <silaevegor@hotmail.com>
2016-04-04 15:24:01 +0300

    Add houndci recommend

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,11 +1,10 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process :resize_to_fit => [360, 360]
+  process resize_to_fit: [360, 360]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -47,5 +46,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
