f388646e92d27e188a53b92a6e2dd4c68f35fc80
Egor G. Silaev <silaevegor@hotmail.com>
2016-04-04 16:31:32 +0300

    Added spec test for picture upload

---
+++ spec/features/picture_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe 'Check picture upload' do
+  let!(:user) { create(:user, email: "test@test.ru", password: "test", password_confirmation: "test") }
+
+  it "Picture must be load success" do
+    visit root_path
+    click_link "Войти"
+    fill_in :email, with: "test@test.ru"
+    fill_in :password, with: "test"
+    click_button "Войти"
+    click_link "Добавить карточку"
+    fill_in :card_original_text, with: "test"
+    fill_in :card_translated_text, with: "тест"
+    attach_file :card_avatar, "#{Rails.root}/spec/support/images/test.jpg"
+    click_button "Создать"
+    visit root_path
+    expect(page).to have_css("img[src*='test.jpg']")
+  end
+end
