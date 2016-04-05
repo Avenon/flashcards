31a497965c28e607ccf3290a49037d969565a652
Egor G. Silaev <silaevegor@hotmail.com>
2016-04-05 18:13:40 +0300

    Changed rspec tests

--- spec/features/decks_spec.rb
+++ spec/features/decks_spec.rb
@@ -4,29 +4,26 @@ describe 'Check activate/deactivate decks' do
   let!(:user) { create(:user, email: "test@test.ru", password: "test", password_confirmation: "test") }
   let!(:deck) { FactoryGirl.create(:deck, user_id: user.id, active: false) }
 
-  it "Deck should be activate" do
+  it "Field active of the deck should be changed status on true, after click activate" do
     visit root_path
     click_link "Войти"
     fill_in :email, with: "test@test.ru"
     fill_in :password, with: "test"
     click_button "Войти"
-    click_link "Все колоды"
-    click_link "изменить"
+    visit edit_deck_path(deck)
     click_link "Активировать"
     expect(page).to have_content("true")
   end
 
-  it "Deck should be deactivate" do
+  it "Field active of the deck should be changed status on false, after click deactivate" do
     visit root_path
     click_link "Войти"
     fill_in :email, with: "test@test.ru"
     fill_in :password, with: "test"
     click_button "Войти"
-    click_link "Все колоды"
-    click_link "изменить"
+    visit edit_deck_path(deck)
     deck.active = true
     click_link "Неактивная"
-    puts page.body
     expect(page).to have_content("false")
   end
 end
