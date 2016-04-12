3e7341930c3acd78dc59c482ac7bf52dedece2ca
Egor G. Silaev <silaevegor@hotmail.com>
2016-04-12 13:28:29 +0300

    Added unit spec for card

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -20,8 +20,31 @@ RSpec.describe Card, type: :model do
 
   it "review date must be increase to 12 hours" do
     card.increase_review_date(true)
-    # expect(card.review_date.strftime('%d/%m/%Y')).to eq((Time.now + 12.hours) #.strftime('%d/%m/%Y'))
     expect(card.review_date.strftime('%H:%M')).to eq((Time.now.utc + 12.hours).strftime('%H:%M'))
+  end
+
+  it "review date must be increase to 3 days" do
+    card = Card.new(original_text: "house", level: 1, attempt: 1, review_date: Time.now)
+    card.increase_review_date(true)
+    expect(card.review_date.strftime('%d/%m/%Y')).to eq((Time.now.utc + 3.days).strftime('%d/%m/%Y'))
+  end
+
+  it "review date must be increase to 1 week" do
+    card = Card.new(original_text: "house", level: 2, attempt: 1, review_date: Time.now)
+    card.increase_review_date(true)
+    expect(card.review_date.strftime('%d/%m/%Y')).to eq((Time.now.utc + 1.weeks).strftime('%d/%m/%Y'))
+  end
+
+  it "review date must be increase to 2 weeks" do
+    card = Card.new(original_text: "house", level: 3, attempt: 1, review_date: Time.now)
+    card.increase_review_date(true)
+    expect(card.review_date.strftime('%d/%m/%Y')).to eq((Time.now.utc + 2.weeks).strftime('%d/%m/%Y'))
+  end
+
+  it "review date must be increase to 1 month" do
+    card = Card.new(original_text: "house", level: 4, attempt: 1, review_date: Time.now)
+    card.increase_review_date(true)
+    expect(card.review_date.strftime('%d/%m/%Y')).to eq((Time.now.utc + 1.month).strftime('%d/%m/%Y'))
   end
 
   it "level must be increase up to 1 when result translate is true" do
